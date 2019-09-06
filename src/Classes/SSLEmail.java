3f2b3acaf97755a4065aacd2ce3738275147ee49
SailentzZz <mishatravin99@gmail.com>
2019-09-06 05:36:58 +0300

    Changes

--- src/Classes/SSLEmail.java
+++ src/Classes/SSLEmail.java
@@ -53,10 +53,10 @@ public class SSLEmail {
 		System.out.println("SSLEmail Start");
 		Properties props = new Properties();
 		props.put("mail.smtp.host", "smtp.gmail.com"); //SMTP Host
-		props.put("mail.smtp.socketFactory.port", "465"); //SSL Port
+		props.put("mail.smtp.socketFactory.port", "587"); //SSL Port
 		props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory"); //SSL Factory Class
 		props.put("mail.smtp.auth", "true"); //Enabling SMTP Authentication
-		props.put("mail.smtp.port", "465"); //SMTP Port
+		props.put("mail.smtp.port", "587"); //SMTP Port
 		
 		Authenticator auth = new Authenticator() {
 			//override the getPasswordAuthentication method
